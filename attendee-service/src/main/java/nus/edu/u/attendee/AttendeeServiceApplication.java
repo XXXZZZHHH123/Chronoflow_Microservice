5be71d5a259a05ce8078bd54c01f801c93466ea3
YuuKiSSa <e1241986@u.nus.edu>
2025-10-12 11:20:33 +0800

    1.Dubbo

--- attendee-service/src/main/java/nus/edu/u/attendee/AttendeeServiceApplication.java
+++ attendee-service/src/main/java/nus/edu/u/attendee/AttendeeServiceApplication.java
@@ -1,5 +1,6 @@
 package nus.edu.u.attendee;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2025-10-05
  */
 @SpringBootApplication
+@EnableDubbo
 public class AttendeeServiceApplication {
 
     public static void main(String[] args) {

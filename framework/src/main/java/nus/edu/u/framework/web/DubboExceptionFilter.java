ee7cb528d9e35ac3904e067fe20b90a3682a7059
YuuKiSSa <e1241986@u.nus.edu>
2025-10-17 22:59:29 +0800

    1.exception handler

--- framework/src/main/java/nus/edu/u/framework/web/DubboExceptionFilter.java
+++ framework/src/main/java/nus/edu/u/framework/web/DubboExceptionFilter.java
@@ -1,5 +1,8 @@
 package nus.edu.u.framework.web;
 
+import static org.apache.dubbo.common.constants.LoggerCodeConstants.CONFIG_FILTER_VALIDATION_EXCEPTION;
+
+import java.lang.reflect.Method;
 import nus.edu.u.common.exception.ServiceException;
 import org.apache.dubbo.common.constants.CommonConstants;
 import org.apache.dubbo.common.extension.Activate;
@@ -16,17 +19,14 @@ import org.apache.dubbo.rpc.RpcException;
 import org.apache.dubbo.rpc.service.GenericService;
 import org.apache.dubbo.rpc.support.RpcUtils;
 
-import java.lang.reflect.Method;
-
-import static org.apache.dubbo.common.constants.LoggerCodeConstants.CONFIG_FILTER_VALIDATION_EXCEPTION;
-
 /**
  * @author Lu Shuwen
  * @date 2025-10-17
  */
 @Activate(group = CommonConstants.PROVIDER)
 public class DubboExceptionFilter implements Filter, Filter.Listener {
-    private ErrorTypeAwareLogger logger = LoggerFactory.getErrorTypeAwareLogger(DubboExceptionFilter.class);
+    private ErrorTypeAwareLogger logger =
+            LoggerFactory.getErrorTypeAwareLogger(DubboExceptionFilter.class);
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
@@ -46,7 +46,11 @@ public class DubboExceptionFilter implements Filter, Filter.Listener {
 
                 // 如果签名中出现异常，则直接抛出
                 try {
-                    Method method = invoker.getInterface().getMethod(RpcUtils.getMethodName(invocation), invocation.getParameterTypes());
+                    Method method =
+                            invoker.getInterface()
+                                    .getMethod(
+                                            RpcUtils.getMethodName(invocation),
+                                            invocation.getParameterTypes());
                     Class<?>[] exceptionClasses = method.getExceptionTypes();
                     for (Class<?> exceptionClass : exceptionClasses) {
                         if (exception.getClass().equals(exceptionClass)) {
@@ -58,20 +62,35 @@ public class DubboExceptionFilter implements Filter, Filter.Listener {
                 }
 
                 // 对于在方法的签名中找不到的异常，请在服务器的日志中打印ERROR消息。
-                logger.error(CONFIG_FILTER_VALIDATION_EXCEPTION, "", "",
-                        "Got unchecked and undeclared exception which called by " + RpcContext.getServiceContext().getRemoteHost() +
-                                ". service: " + invoker.getInterface().getName() + ", method: " + RpcUtils.getMethodName(invocation) +
-                                ", exception: " + exception.getClass().getName() + ": " + exception.getMessage(), exception);
+                logger.error(
+                        CONFIG_FILTER_VALIDATION_EXCEPTION,
+                        "",
+                        "",
+                        "Got unchecked and undeclared exception which called by "
+                                + RpcContext.getServiceContext().getRemoteHost()
+                                + ". service: "
+                                + invoker.getInterface().getName()
+                                + ", method: "
+                                + RpcUtils.getMethodName(invocation)
+                                + ", exception: "
+                                + exception.getClass().getName()
+                                + ": "
+                                + exception.getMessage(),
+                        exception);
 
                 // 如果异常类和接口类在同一个jar文件中，则直接抛出。
                 String serviceFile = ReflectUtils.getCodeBase(invoker.getInterface());
                 String exceptionFile = ReflectUtils.getCodeBase(exception.getClass());
-                if (serviceFile == null || exceptionFile == null || serviceFile.equals(exceptionFile)) {
+                if (serviceFile == null
+                        || exceptionFile == null
+                        || serviceFile.equals(exceptionFile)) {
                     return;
                 }
                 // 如果是JDK异常，则直接抛出
                 String className = exception.getClass().getName();
-                if (className.startsWith("java.") || className.startsWith("javax.") || className.startsWith("jakarta.")) {
+                if (className.startsWith("java.")
+                        || className.startsWith("javax.")
+                        || className.startsWith("jakarta.")) {
                     return;
                 }
                 // 如果是dubbo异常，则直接抛出
@@ -87,20 +106,42 @@ public class DubboExceptionFilter implements Filter, Filter.Listener {
                 // 否则，请使用RuntimeException进行包装并返回到客户端
                 appResponse.setException(new RuntimeException(StringUtils.toString(exception)));
             } catch (Throwable e) {
-                logger.warn(CONFIG_FILTER_VALIDATION_EXCEPTION, "", "",
-                        "Fail to ExceptionFilter when called by " + RpcContext.getServiceContext().getRemoteHost() +
-                                ". service: " + invoker.getInterface().getName() + ", method: " + RpcUtils.getMethodName(invocation) +
-                                ", exception: " + e.getClass().getName() + ": " + e.getMessage(), e);
+                logger.warn(
+                        CONFIG_FILTER_VALIDATION_EXCEPTION,
+                        "",
+                        "",
+                        "Fail to ExceptionFilter when called by "
+                                + RpcContext.getServiceContext().getRemoteHost()
+                                + ". service: "
+                                + invoker.getInterface().getName()
+                                + ", method: "
+                                + RpcUtils.getMethodName(invocation)
+                                + ", exception: "
+                                + e.getClass().getName()
+                                + ": "
+                                + e.getMessage(),
+                        e);
             }
         }
     }
 
     @Override
     public void onError(Throwable e, Invoker<?> invoker, Invocation invocation) {
-        logger.error(CONFIG_FILTER_VALIDATION_EXCEPTION, "", "",
-                "Got unchecked and undeclared exception which called by " + RpcContext.getServiceContext().getRemoteHost() +
-                        ". service: " + invoker.getInterface().getName() + ", method: " + RpcUtils.getMethodName(invocation) +
-                        ", exception: " + e.getClass().getName() + ": " + e.getMessage(), e);
+        logger.error(
+                CONFIG_FILTER_VALIDATION_EXCEPTION,
+                "",
+                "",
+                "Got unchecked and undeclared exception which called by "
+                        + RpcContext.getServiceContext().getRemoteHost()
+                        + ". service: "
+                        + invoker.getInterface().getName()
+                        + ", method: "
+                        + RpcUtils.getMethodName(invocation)
+                        + ", exception: "
+                        + e.getClass().getName()
+                        + ": "
+                        + e.getMessage(),
+                e);
     }
 
     // For test purpose

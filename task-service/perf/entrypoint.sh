01eb4bfbea1cfd938a742005289fb0b0d12f107d
XXXZZZHHH123 <e1133135@u.nus.edu>
2025-10-20 10:01:05 +0800

    fix issues

--- task-service/perf/entrypoint.sh
+++ task-service/perf/entrypoint.sh
@@ -9,7 +9,7 @@ fi
 export MAVEN_OPTS="${MAVEN_OPTS:--Xms256m -Xmx512m -XX:+UseContainerSupport}"
 
 declare -a args
-args=(mvn -pl task-service -am gatling:test -Dgatling.skip=false "-DtaskService.baseUrl=${TASK_SERVICE_BASE_URL}")
+args=(mvn -pl task-service -am io.gatling:gatling-maven-plugin:test -Dgatling.skip=false "-DtaskService.baseUrl=${TASK_SERVICE_BASE_URL}")
 
 if [[ -n "${TASK_SERVICE_LOGIN_PATH:-}" ]]; then
   args+=("-DtaskService.loginPath=${TASK_SERVICE_LOGIN_PATH}")
